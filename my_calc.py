96f5d148c6deab06cab68783815cab59cc3ae2ad
NikitaTrushinQA <nikitatrushinqa@gmail.com>
2023-04-19 18:49:52 +0300

    add my_calc.py

---
+++ my_calc.py
@@ -0,0 +1,16 @@
+
+class Methods:
+    def mult(self,x,y):
+        return x*y
+
+    def summator(self,x,y):
+        return x+y
+
+    def div(self,x,y):
+        try:
+            return x/y
+        except :
+            return 'Error'
+
+    def dif(self,x,y):
+        return x-y
